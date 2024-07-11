666ecaf652d2e445976f5cb4c87b3150bdaa0e63
Kan-Anek Tantitayapong <maikt1228@gmail.com>
2024-07-11 18:24:02 +0700

    feat: added primitive roots

--- src/prim_roots/mod.rs
+++ src/prim_roots/mod.rs
@@ -22,16 +22,39 @@ pub fn pow_mod(a: u64, m: u64, n: u64) -> u64 {
     helper(a, m, n, 1)
 }
 
-pub fn ord_mod_n(a: u64, n: u64) -> u64 {
+fn ord_mod_n(a: u64, n: u64) -> u64 {
     if a%n == 1 {
         return 0
     }
     let ord = (1..n).into_par_iter()
         .find_first(|&num| pow_mod(a, num, n) == 1);
-    match ord {
-        None => panic!("Element {} has infinite order", a),
-        Some(k) => k
+    ord.unwrap_or_else(|| u64::MAX)
+}
+
+pub fn is_prim_root(a: u64, n: u64) -> bool {
+    ord_mod_n(a, n) == euler_phi(n)
+}
+
+fn has_prim_root(n: u64) -> bool {
+    if n%2 == 0 {
+        if par_prime_factors(n).len() == 1 { n==2 || n==4 }
+        else { par_prime_factors(n/2).len() == 1 }
+    }
+    else {
+        n==1 || n==2 || n==4 || par_prime_factors(n).len() == 1
     }
 }
 
-
+pub fn prim_root(n: u64) -> u64 {
+    if n==1 {return 0}
+    if n==2 {return 1}
+    if n==4 {return 3}
+    if has_prim_root(n) {
+        (1..n).into_par_iter()
+            .find_first(|&num| is_prim_root(num, n))
+            .unwrap()
+    }
+    else {
+        panic!("There is no primitive root modulo {}", n)
+    }
+}
